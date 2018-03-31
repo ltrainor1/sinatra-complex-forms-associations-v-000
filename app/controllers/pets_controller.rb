ec77b799511c0630cd9fb4d4a045792ee5fac333
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-31 01:38:38 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -31,7 +31,7 @@ require 'pry'
       @pet = Pet.update(params[:id], {name: params[:pet_name], owner_id: @owner.id})
     else
       @pet = Pet.update(params[:id], {name: params[:pet_name], owner_id: params[:id]})
-    end 
+    end
     redirect to "pets/#{@pet.id}"
   end
 
