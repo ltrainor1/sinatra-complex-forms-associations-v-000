747b526de33b962bbc3fb4329ba1bfd385350524
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-31 01:21:38 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,26 +1,30 @@
 class PetsController < ApplicationController
-
+require 'pry'
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
-    redirect to "pets/#{@pet.id}"
+  post '/pets' do
+    if params[:owner_name] != ""
+      @owner = Owner.create(name: params[:owner_name])
+      @pet = Pet.create(name: params[:pet][:name], owner_id: @owner.id)
+    else
+      @pet = Pet.create(name: params[:pet][:name], owner_id: params[:owner][:ids][0])
+    end
+    redirect "/pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
-    @pet = Pet.find(params[:id])
+  get '/pets/:id' do
+    @pet = Pet.find(params[:id].to_i)
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
-
+  post '/pets/:id' do
     redirect to "pets/#{@pet.id}"
   end
 end
